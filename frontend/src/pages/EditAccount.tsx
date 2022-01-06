7916781bdf464e3b35c82c1290f13ae6f73bfd11
SmartForger <23446619+SmartForger@users.noreply.github.com>
2022-01-06 12:45:55 -0800

    Add account form

--- frontend/src/pages/EditAccount.tsx
+++ frontend/src/pages/EditAccount.tsx
@@ -1,7 +1,74 @@
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import account from "../services/account";
+import * as Yup from "yup";
+
+const AccountSchema = Yup.object().shape({
+  name: Yup.string().required("Name is required"),
+  email: Yup.string().email("Invalid format").required("Email is required"),
+  balance: Yup.number()
+    .positive("Balance should be a positive number")
+    .required("Balance is required"),
+});
+
 export const EditAccount = () => {
+  const user = account.getUser();
+
   return (
     <div className="EditAccount">
-      Edit Account Details
+      <h3>Update Account</h3>
+      <Formik
+        initialValues={{ email: "", password: "" }}
+        validationSchema={AccountSchema}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+          }, 400);
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form className="edit-account-form">
+            <div className="control">
+              <label htmlFor="name">Name:</label>
+              <Field id="name" name="name" className="input" />
+            </div>
+            <ErrorMessage className="field-error" name="name" component="div" />
+            <div className="control">
+              <label htmlFor="email">Email:</label>
+              <Field id="email" type="email" name="email" className="input" />
+            </div>
+            <ErrorMessage
+              className="field-error"
+              name="email"
+              component="div"
+            />
+            <div className="control">
+              <label htmlFor="balance">Balance:</label>
+              <Field
+                id="balance"
+                name="balance"
+                className="input"
+                type="number"
+              />
+            </div>
+            <ErrorMessage
+              className="field-error"
+              name="balance"
+              component="div"
+            />
+            <div className="actions">
+              <button className="button" type="submit" disabled={isSubmitting}>
+                Update
+              </button>
+              {user && (
+                <button className="button" disabled={isSubmitting}>
+                  Cancel
+                </button>
+              )}
+            </div>
+          </Form>
+        )}
+      </Formik>
     </div>
-  )
-}
+  );
+};
